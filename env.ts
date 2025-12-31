dc8de8f5328e1293049ce1b5771b18f4037c6f33
arekgotfryd <arekgotfryd@g.pl>
2025-12-31 10:45:35 +0100

    fix unit tests coverage command

--- env.ts
+++ env.ts
@@ -2,17 +2,9 @@ import { env as loadEnv } from 'custom-env'
 import { z } from 'zod'
 
 process.env.APP_STAGE = process.env.APP_STAGE || 'dev'
-
 const isProduction = process.env.APP_STAGE === 'production'
-const isDevelopment = process.env.APP_STAGE === 'dev' 
-const isTest = process.env.APP_STAGE === 'test'
-
-// Load .env file
-if (isDevelopment) {
-  loadEnv()
-} else if (isTest) {
-  loadEnv('test')
-}
+//load environment variables from .env file
+loadEnv()
 
 // Define the schema with environment-specific requirements
 const envSchema = z.object({
